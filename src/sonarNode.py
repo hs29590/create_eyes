bf5f56f7484a60da6022554f4d42f7cac7857aa6
Harshit Sureka <hs29590@gmail.com>
2017-12-01 10:51:14 +0100

    making sonarNode work

--- src/sonarNode.py
+++ src/sonarNode.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import roslib
 roslib.load_manifest('create_eyes')
+from std_msgs.msg import String
 import sys
 import rospy
 import time
@@ -9,20 +10,8 @@ import pigpio
 
 
 class SonarClass:
-
-    self.TRIGGER=19
-    self.ECHO1=21
-    self.ECHO2=26
-    self.ECHO3=20
-
-    self.high_tick1 = None # global to hold high tick.
-    self.high_tick2 = None # global to hold high tick.
-    self.high_tick3 = None # global to hold high tick.
-
-    self.state_pub = rospy.Publisher('create_state', String, queue_size=1)
-    self.desired_state = "Drive";
    
-    def cbfunc1(gpio, level, tick):
+    def cbfunc1(self,gpio, level, tick):
        if level == 0: # echo line changed from high to low.
           if self.high_tick1 is not None:
              echo = pigpio.tickDiff(self.high_tick1, tick)
@@ -33,7 +22,7 @@ class SonarClass:
        else:
           self.high_tick1 = tick
 
-    def cbfunc2(gpio, level, tick):
+    def cbfunc2(self,gpio, level, tick):
        if level == 0: # echo line changed from high to low.
           if self.high_tick2 is not None:
              echo = pigpio.tickDiff(self.high_tick2, tick)
@@ -44,7 +33,7 @@ class SonarClass:
        else:
           self.high_tick2 = tick
 
-    def cbfunc3(gpio, level, tick):
+    def cbfunc3(self,gpio, level, tick):
        if level == 0: # echo line changed from high to low.
           if self.high_tick3 is not None:
              echo = pigpio.tickDiff(self.high_tick3, tick)
@@ -57,6 +46,18 @@ class SonarClass:
           
     def __init__ (self):
 
+        self.TRIGGER=19
+        self.ECHO1=21
+        self.ECHO2=26
+        self.ECHO3=20
+    
+        self.high_tick1 = None # global to hold high tick.
+        self.high_tick2 = None # global to hold high tick.
+        self.high_tick3 = None # global to hold high tick.
+    
+        self.state_pub = rospy.Publisher('create_state', String, queue_size=1)
+        self.desired_state = "Drive";
+
         self.pi = pigpio.pi() # Connect to local Pi.
 
         self.pi.set_mode(self.TRIGGER, pigpio.OUTPUT)
@@ -64,9 +65,9 @@ class SonarClass:
         self.pi.set_mode(self.ECHO2, pigpio.INPUT)
         self.pi.set_mode(self.ECHO3, pigpio.INPUT)
 
-        self.cb1 = pi.callback(self.ECHO1, pigpio.EITHER_EDGE, self.cbfunc1)
-        self.cb2 = pi.callback(self.ECHO2, pigpio.EITHER_EDGE, self.cbfunc2)
-        self.cb3 = pi.callback(self.ECHO3, pigpio.EITHER_EDGE, self.cbfunc3)
+        self.cb1 = self.pi.callback(self.ECHO1, pigpio.EITHER_EDGE, self.cbfunc1)
+        self.cb2 = self.pi.callback(self.ECHO2, pigpio.EITHER_EDGE, self.cbfunc2)
+        self.cb3 = self.pi.callback(self.ECHO3, pigpio.EITHER_EDGE, self.cbfunc3)
    
     def __del__(self):
         self.cb1.cancel() # Cancel callback.
@@ -74,16 +75,19 @@ class SonarClass:
         self.cb3.cancel() # Cancel callback.
         self.pi.stop() # Close connection to Pi 
 
+    def triggerAndPublish(self):
+        self.desired_state = "Drive";
+        self.pi.gpio_trigger(self.TRIGGER, 10);
+        time.sleep(0.2);
+        self.state_pub.publish(self.desired_state);
+
 
 def main(args):
   rospy.init_node('sonar_node_create', anonymous=True)
   sc = SonarClass()
-  rospy.spin();
-  while(True):
-    sc.desired_state = "Drive";
-    pi.gpio_trigger(TRIGGER, 10)
-    time.sleep(0.2);
-    self.state_pub.publish(sc.desired_state);
+#  rospy.spin();
+  while not rospy.is_shutdown():
+    sc.triggerAndPublish(); #this sleeps for 0.2s internally
         
 if __name__ == '__main__':
     main(sys.argv)
