4f5b4b8b83abc14c0af3921b18dba14f747e29c9
Harshit Sureka <harshit.sureka@gmail.com>
2017-12-06 16:33:37 +0530

    turnCreate working and gui basic made. Need to make into ros node

---
+++ src/gui.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+from glob import glob
+import time
+import sys
+import math
+from tkinter import *
+from tkinter import ttk
+import _thread
+
+
+class LineFollowerGUI():
+    
+    def __init__(self):
+        self.status = "Started"
+        self.battery = "N/A"
+        self.facingTowardsA = False;
+
+        self.root = Tk()
+        self.root.title("GUI")
+        
+        framew = 500; # root w
+        frameh = 400; # root h
+        screenw = self.root.winfo_screenwidth();
+        screenh = self.root.winfo_screenheight();
+        posx = (screenw/2) - (framew/2);
+        posy = (screenh/2) - (frameh/2);
+        self.root.geometry( "%dx%d+%d+%d" % (framew,frameh,posx,posy))
+
+        self.currentStatus = StringVar();
+        self.currentStatus.set('Starting');
+
+        self.batteryStatus = StringVar();
+        self.batteryStatus.set('95%');
+        
+        self.mainframe = ttk.Frame(self.root, padding="10 10 30 30", height=400, width=500)
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.mainframe.columnconfigure(0, weight=50,minsize=50)
+        self.mainframe.rowconfigure(0, weight=50,minsize=50)
+
+        self.buttonStyle = ttk.Style()
+        self.buttonStyle.configure('my.TButton', font=('Helvetica', 18))
+        
+        self.batteryLabel = ttk.Label(self.mainframe, textvariable=self.batteryStatus, font=('Helvetica',12));
+        self.batteryLabel.grid(row=1, column=1);
+
+        self.statusLabel = ttk.Label(self.mainframe, textvariable=self.currentStatus, font=('Helvetica',12));
+        self.statusLabel.grid(row=1, column=0);
+
+        ttk.Button(self.mainframe, text="Go To A", style='my.TButton', command=self.goToA, width=16).grid(row=2, rowspan=2, column=0, pady=25)
+        ttk.Button(self.mainframe, text="Go To B/DOCK", style='my.TButton', command=self.goToB, width=16).grid(row=2, rowspan=2, column=1, pady=25)
+        ttk.Button(self.mainframe, text="STOP", style='my.TButton', command=self.Stop, width=16).grid(row=4, rowspan=3, column=0, columnspan=3, pady=5)
+        
+        self.root.after(1000, self.updateLabel);
+    
+    def goToA(self):
+        print("if not facing towards A ( turn 180), start following line, publish, turn, or drive state");
+        self.currentStatus.set("Going to A");
+        self.batteryStatus.set("92%");
+
+    def goToB(self):
+        print("if facing towards A (turn 180), start following Line, publish, turn or follow line state");
+        self.currentStatus.set("Going to B");
+        while(True):
+            self.batteryStatus.set("90%");
+        
+
+    def Stop(self):
+        print("stop");
+        self.currentStatus.set("Stopped");
+
+    def updateLabel(self):
+        self.statusLabel.update_idletasks();
+        self.batteryLabel.update_idletasks();
+    
+        self.root.update_idletasks();
+        self.root.after(200, self.updateLabel);
+
+    def __del__(self):
+        pass;
+gui = LineFollowerGUI();
+gui.root.mainloop();
+
--- src/turnCreate180.py
+++ src/turnCreate180.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import tf
+import time
 import roslib
 roslib.load_manifest('create_eyes')
 import sys
@@ -7,6 +9,7 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32
+from nav_msgs.msg import Odometry
 
 import numpy
 import math
@@ -17,23 +20,66 @@ class DriveCreate2:
   def __init__(self):
 
     self.err_sub = rospy.Subscriber('line_error', Float32, self.errCallback)
- 
-    self.TimeToHalfAfterNoErrRecd = rospy.Duration(1); #Halt if we don't recv. a cmd_msg for these many seconds
- 
+    self.odom_sub = rospy.Subscriber('odom', Odometry, self.odomCallback)
+    self.yaw = None;
+    self.last_odom = Odometry();
+
+
     self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     self.twist = Twist()
-    self.lastErrorMsgRecdTime = rospy.Time.now() - self.TimeToHalfAfterNoErrRecd;
 
-  def errCallback(self,err):
-    if(err.data == -1000.0):
-        self.twist.linear.x = 0.0
-        self.twist.angular.z = 0.0
-        self.cmd_vel_pub.publish(self.twist)
-    else:
-        self.lastErrorMsgRecdTime = rospy.Time.now();
-        self.twist.linear.x = 0.2
-        self.twist.angular.z = -float(err.data) / 100
-        self.cmd_vel_pub.publish(self.twist)
+  def errCallback(self,msg):
+      alpha = msg.data
+      theta = self.yaw;
+      rollOverReqd = False;
+      if(alpha > 0):
+         desired_angle = theta + alpha;
+         if(desired_angle > math.pi):
+            desired_angle = desired_angle - 2*math.pi;
+            rollOverReqd = True;
+         while(True):
+            self.twist.linear.x = 0;
+            self.twist.angular.z = 0.5;
+            self.cmd_vel_pub.publish(self.twist)
+            print("Turning: " + str(theta) + " to get to " + str(desired_angle));
+            if((theta) > (desired_angle)):
+                if(not rollOverReqd):
+                    break;
+                else:
+                    if(theta < 0):
+                        break;
+                    
+            theta = self.yaw
+
+         self.twist.angular.z = 0;
+         self.cmd_vel_pub.publish(self.twist);
+      
+      elif(alpha < 0):
+          desired_angle = theta + alpha;
+          if(desired_angle < -math.pi):
+              desired_angle = desired_angle + 2*math.pi;
+              rollOverReqd = True;
+          while(True):
+              self.twist.linear.x = 0;
+              self.twist.angular.z = -0.5;
+              self.cmd_vel_pub.publish(self.twist);
+              print("Turning: " + str(theta) + " to get to " + str(desired_angle));
+              if(theta < desired_angle):
+                  if(not rollOverReqd):
+                    break;
+                  else:
+                    if(theta > 0):
+                      break;
+              theta = self.yaw
+
+          self.twist.angular.z = 0;
+          self.cmd_vel_pub.publish(self.twist);
+
+  def odomCallback(self,msg):
+    self.last_odom = msg;
+    quaternion = (msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z,msg.pose.pose.orientation.w)
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    self.yaw = euler[2]
 
 def main(args):
   rospy.init_node('drive_create2', anonymous=True)
