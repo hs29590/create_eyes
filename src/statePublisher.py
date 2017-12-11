097649e00458429c07fc2416669f3cd321c3eb06
Harshit Sureka <harshit.sureka@gmail.com>
2017-12-11 16:37:41 +0530

    adding heading

--- src/statePublisher.py
+++ src/statePublisher.py
@@ -69,6 +69,9 @@ class Create2StatePublisher:
     self.batteryStatus = StringVar();
     self.batteryStatus.set('na%');
     
+    self.headingStatus = StringVar();
+    self.headingStatus.set('NA');
+
     self.mainframe = ttk.Frame(self.root, padding="10 10 30 30", height=400, width=500)
     self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
     self.mainframe.columnconfigure(0, weight=50,minsize=50)
@@ -83,6 +86,9 @@ class Create2StatePublisher:
     self.statusLabel = ttk.Label(self.mainframe, textvariable=self.currentStatus, font=('Helvetica',12));
     self.statusLabel.grid(row=1, column=0);
 
+    self.headLabel = ttk.Label(self.mainframe, textvariable=self.headingStatus, font=('Helvetica',12));
+    self.headLabel.grid(row=0,column=1);
+
     ttk.Button(self.mainframe, text="Go To A", style='my.TButton', command=self.goToA, width=16).grid(row=2, rowspan=2, column=0, pady=25)
     ttk.Button(self.mainframe, text="Go To B/DOCK", style='my.TButton', command=self.goToB, width=16).grid(row=2, rowspan=2, column=1, pady=25)
     ttk.Button(self.mainframe, text="STOP", style='my.TButton', command=self.Stop, width=16).grid(row=4, rowspan=3, column=0, columnspan=3, pady=5)
@@ -93,19 +99,19 @@ class Create2StatePublisher:
  
   def goToA(self):
       #print("if not facing towards A ( turn 180), start following line, publish, turn, or drive state");
-      print("GotoA");
+      rospy.loginfo("GotoA");
       if(self.last_gui_state != "GoToA"):
 #      self.last_gui_state = "GoToA";
           self.guiButtonUpdate("GoToA");
   def goToB(self):
-      print("GoToB");
+      rospy.loginfo("GoToB");
       #print("if facing towards A (turn 180), start following Line, publish, turn or follow line state");
       if(self.last_gui_state != "GoToB"):
 #         self.last_gui_state = "GoToB";
           self.guiButtonUpdate("GoToB");
 
   def Stop(self):
-      print("Stop");
+      rospy.loginfo("Stop");
       if(self.last_gui_state != "Stop"):
 #         self.last_gui_state = "Stop";
           self.guiButtonUpdate("Stop");
@@ -115,9 +121,11 @@ class Create2StatePublisher:
 #          call(["rostopic", "pub", "-1", "/gui_state", "std_msgs/String", self.current_state])
 #          self.last_state_published = self.current_state;
       self.currentStatus.set(self.last_gui_state);
+      self.headingStatus.set(self.heading);
       self.statusLabel.update_idletasks();
       self.batteryLabel.update_idletasks();
-    
+      self.headLabel.update_idletasks();
+
       self.root.update_idletasks();
       self.root.after(200, self.updateLabel);
  
@@ -128,7 +136,7 @@ class Create2StatePublisher:
   def publish(self,msg):
       self.master_state_pub.publish(msg);
       self.last_state_published = msg;
-      print("Published: " + msg);
+      rospy.loginfo("Published: " + msg);
 
   def turnAround(self):
       self.publish("CONTROLLER_Turn");
@@ -167,7 +175,7 @@ class Create2StatePublisher:
           self.publish("CONTROLLER_Stop");
 
       else:
-          print("Unknown state: " + msg);
+          rospy.logwarn("Unknown state: " + msg);
     
       self.last_gui_state = msg;
 
