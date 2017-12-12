84369c8c15d3dcec1652870f6d8f72e08a71ace8
Harshit Sureka <harshit.sureka@gmail.com>
2017-12-12 09:34:03 +0530

    adding rospyloginfo instead of print

--- src/followLine2.py
+++ src/followLine2.py
@@ -106,7 +106,7 @@ def main(args):
   try:
     rospy.spin()
   except KeyboardInterrupt:
-    print("Shutting down")
+    rospy.loginfo("Shutting down")
   cv2.destroyAllWindows()
 
 if __name__ == '__main__':
